76d5de11a1d7f0f802306cda9447d51b8e577811
Francisco Campos <franciiscocampos170@gmail.com>
2020-10-07 18:55:06 +0100

    contact-form logic added

--- app/Http/Livewire/ContactForm.php
+++ app/Http/Livewire/ContactForm.php
@@ -3,9 +3,39 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Mail;
 
 class ContactForm extends Component
 {
+    public $name, $email, $comment, $success;
+    protected $rules = [
+        'name' => 'required',
+        'email' => 'required|email',
+        'comment' => 'required|min:5'
+    ];
+    public function contactFormSubmit()
+    {
+        $contact = $this->validate();
+        Mail::send('email',
+        array(
+            'name' => $this->name,
+            'email' => $this->email,
+            'comment' => $this->comment
+        ), function($message){
+            $message->from('franciiscocampos170@gmail.com');
+            $message->to('dev.franciscocampos@gmail.com', 'Francisco Campos')->subject('Livewire Contact Form');
+        });
+        $this->message = 'Thank you for reaching out to us!';
+        $this->clearFields();
+    }
+
+    private function clearFields()
+    {
+        $this->name = '';
+        $this->email = '';
+        $this->comment = '';
+    }
+    
     public function render()
     {
         return view('livewire.contact-form');
